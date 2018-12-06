eefc926b6bad0bb35dab40c0f49cedaef152898b
Sergio Garre <sergio.garre@sheddapp.com>
2018-12-06 01:35:34 +0100

    LoadingView refactor

---
+++ incredibleMovie/Components/LoadingView/LoadingResources.swift
@@ -0,0 +1,14 @@
+//
+//  LoadingResources.swift
+//  incredibleMovie
+//
+//  Created by Sergio Garre on 06/12/2018.
+//  Copyright © 2018 Sergio Garre. All rights reserved.
+//
+
+import Foundation
+
+struct LoadingResources {
+    static let nibName = "LoadingView"
+    static let animationIdentifier = "loader"
+}
--- incredibleMovie/Components/LoadingView/LoadingView.swift
+++ incredibleMovie/Components/LoadingView/LoadingView.swift
@@ -10,9 +10,11 @@ import UIKit
 import Lottie
 
 class LoadingView: UIView {
+    // MARK: - Public properties
     @IBOutlet private var contentView: UIView!
     @IBOutlet weak private var loaderView: UIView!
     
+    // MARK: - UIView
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
@@ -24,24 +26,23 @@ class LoadingView: UIView {
     }
     
     private func commonInit() {
-        Bundle.main.loadNibNamed("LoadingView", owner: self, options: nil)
+        Bundle.main.loadNibNamed(LoadingResources.nibName, owner: self, options: nil)
         addSubview(contentView)
         contentView.frame = self.bounds
         contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
     }
     
     override func awakeFromNib() {
-        let animationView = LOTAnimationView(name: "loader")
+        let animationView = LOTAnimationView(name: LoadingResources.animationIdentifier)
         animationView.frame = loaderView.frame
         loaderView.addSubview(animationView)
-        
         animationView.translatesAutoresizingMaskIntoConstraints = false
         animationView.leadingAnchor.constraint(equalTo: loaderView.leadingAnchor).isActive = true
         animationView.trailingAnchor.constraint(equalTo: loaderView.trailingAnchor).isActive = true
         animationView.topAnchor.constraint(equalTo: loaderView.topAnchor).isActive = true
         animationView.bottomAnchor.constraint(equalTo: loaderView.bottomAnchor).isActive = true
-        
         animationView.loopAnimation = true
+        
         animationView.play()
     }
 }
