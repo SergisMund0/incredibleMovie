636a0555bbe3f34b196de18c14517454cc02ff34
Sergio Garre <sergio.garre@sheddapp.com>
2018-12-06 01:36:34 +0100

    Add pragma marks

--- incredibleMovie/Components/MovieViewCell/MovieViewCell.swift
+++ incredibleMovie/Components/MovieViewCell/MovieViewCell.swift
@@ -10,10 +10,12 @@ import UIKit
 import AlamofireImage
 
 class MovieViewCell: UITableViewCell {
+    // MARK: - Public properties
     @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var subtitleLabel: UILabel!
     
+    // MARK: - UIView
     override func awakeFromNib() {
         backgroundImage.contentMode = .scaleAspectFill
     }
